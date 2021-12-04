047dc45fbee260257d75454cfba9a6db2245cbca
omkmorendha <omkmorendha@gmail.com>
2021-12-04 18:48:28 +0530

    Fix responsive viewport on /home

--- app.js
+++ app.js
@@ -136,7 +136,6 @@ app.post("/login", function (req, res) {
   req.login(user, function (err) {
     if (err) { console.log(err) }
     else {
-
       passport.authenticate("local")(req, res, function () {
         res.redirect("/selectcity")
       })
