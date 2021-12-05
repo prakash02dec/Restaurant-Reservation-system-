2356169d35a79c1687071b2d76950bab299faf7c
sudeepto147 <88759739+sudeepto147@users.noreply.github.com>
2021-12-05 14:29:06 +0530

    Add files via upload

---
+++ user_profile/scripts/homeColor.js
@@ -0,0 +1,6 @@
+console.log(window.location.href);
+if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature") {
+  $("#home-btn").addClass("_active");
+} else {
+  $("#home-btn").removeClass("_active");
+}
