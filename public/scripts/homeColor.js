a0055c6225890d1477d6b1695d6bda7e4f3c87cf
omkmorendha <omkmorendha@gmail.com>
2021-12-20 12:40:45 +0530

    Update homecolor.js for deployed website

--- public/scripts/homeColor.js
+++ public/scripts/homeColor.js
@@ -1,5 +1,5 @@
 
-if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature") {
+if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature" || window.location.href === "https://resdine.herokuapp.com/" || window.location.href === "https://resdine.herokuapp.com/") {
   $("#home-btn").addClass("_active");
 } else {
   $("#home-btn").removeClass("_active");
