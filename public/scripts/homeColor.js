4971fea60b6f36bc0a438f8c8adee60bf1b05f31
omkmorendha <omkmorendha@gmail.com>
2021-12-20 13:12:47 +0530

    Minor fix to homecolor.js

--- public/scripts/homeColor.js
+++ public/scripts/homeColor.js
@@ -2,7 +2,7 @@ $("#home-btn").removeClass("_active");
 $("#contactus-btn").removeClass("_active");
 $("#booktable-btn").removeClass("_active");
 
-if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature" || window.location.href === "https://resdine.herokuapp.com/" || window.location.href === "https://resdine.herokuapp.com/") {
+if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature" || window.location.href === "https://resdine.herokuapp.com/" || window.location.href === "https://resdine.herokuapp.com/#feature") {
   $("#home-btn").addClass("_active");
 }
 else if (window.location.href === "http://localhost:3000/contactus" || window.location.href === "https://resdine.herokuapp.com/contactus"){
