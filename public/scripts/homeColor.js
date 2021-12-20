96b58fbe7965456bef4b7eb0d77a0e8bc8e2f764
omkmorendha <omkmorendha@gmail.com>
2021-12-20 13:11:20 +0530

    Update homecolor.js for contact us and select city

--- public/scripts/homeColor.js
+++ public/scripts/homeColor.js
@@ -1,6 +1,13 @@
+$("#home-btn").removeClass("_active");
+$("#contactus-btn").removeClass("_active");
+$("#booktable-btn").removeClass("_active");
 
 if (window.location.href === "http://localhost:3000/" || window.location.href === "http://localhost:3000/#feature" || window.location.href === "https://resdine.herokuapp.com/" || window.location.href === "https://resdine.herokuapp.com/") {
   $("#home-btn").addClass("_active");
-} else {
-  $("#home-btn").removeClass("_active");
+}
+else if (window.location.href === "http://localhost:3000/contactus" || window.location.href === "https://resdine.herokuapp.com/contactus"){
+  $("#contactus-btn").addClass("_active");
+}
+else if (window.location.href === "http://localhost:3000/selectcity" || window.location.href === "https://resdine.herokuapp.com/selectcity"){
+  $("#booktable-btn").addClass("_active");
 }
